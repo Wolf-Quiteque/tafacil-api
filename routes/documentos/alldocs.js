b1071cb9980dbffffb569331fdbb16e99165a9f0
Marcio Ragy Daveiga Quiteque <33323596+Wolf-Quiteque@users.noreply.github.com>
2023-01-16 19:03:29 +0100

    Update alldocs.js

--- routes/documentos/alldocs.js
+++ routes/documentos/alldocs.js
@@ -5,7 +5,19 @@ router.get("/", async (req, res) => {
   try {
     const cliente = await clientPromise;
     const db = cliente.db("facilitaverificao");
-    const data = await db.collection("clientes").find({}).toArray();
+    const data = await db.collection("usersClaudio").find({}).toArray();
+
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/users", async (req, res) => {
+  try {
+    const cliente = await clientPromise;
+    const db = cliente.db("facilitaverificao");
+    const data = await db.collection("usersClaudio").find({}).toArray();
 
     res.status(200).json(data);
   } catch (err) {
@@ -18,6 +30,17 @@ router.get("/:bi", async (req, res) => {
     const cliente = await clientPromise;
     const db = cliente.db("facilitaverificao");
     const data = await db.collection("clientes").findOne({ bi: req.params.bi });
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/cluadio/:email", async (req, res) => {
+  try {
+    const cliente = await clientPromise;
+    const db = cliente.db("facilitaverificao");
+    const data = await db.collection("clientes").findOne({ email: req.params.email });
     res.status(200).json(data);
   } catch (err) {
     res.status(500).json(err);

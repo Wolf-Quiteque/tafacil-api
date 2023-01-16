9117d42d24cd643297c487634559a87ddb8ac633
Marcio Ragy Daveiga Quiteque <33323596+Wolf-Quiteque@users.noreply.github.com>
2023-01-16 19:44:09 +0100

    Update user.js

--- routes/authenticacao/user.js
+++ routes/authenticacao/user.js
@@ -49,4 +49,15 @@ router.post("/new/user", async (req, res) => {
   }
 });
 
+router.post("/get/users", async (req, res) => {
+  try {
+    const cliente = await clientPromise;
+    const db = cliente.db("facilitaverificao");
+    const data = await db.collection("usersC").findAll({}).toArray();
+    res.status(200).json("Cadastrado com successo");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
